56181d1f68f8eb878e756d64bdfc99ec593f7202
Kristan Ruíz <itan_07@hotmail.com>
2023-03-22 17:46:02 -0800

    Arreglando bug picturebox más pequeño
    por alguna razón se hizo más pequeño y mandamos tarde la tarea para poder corregir eso. Igual me siento feliz de haber encontrado el error rápidamente.
    
    - El picture box originalmente estaba en 600, 400 pero se redimensionó a 500, 392

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -43,9 +43,10 @@
             // button1
             // 
             this.button1.AccessibleName = "botonnn";
-            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Location = new System.Drawing.Point(27, 11);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(670, 57);
+            this.button1.Size = new System.Drawing.Size(586, 43);
             this.button1.TabIndex = 0;
             this.button1.Text = "Dibujar Vecindario";
             this.button1.UseVisualStyleBackColor = true;
@@ -54,7 +55,8 @@
             // pictureBox1
             // 
             this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.pictureBox1.Location = new System.Drawing.Point(45, 162);
+            this.pictureBox1.Location = new System.Drawing.Point(27, 108);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(600, 400);
             this.pictureBox1.TabIndex = 1;
@@ -62,14 +64,15 @@
             // 
             // numericUpDown1
             // 
-            this.numericUpDown1.Location = new System.Drawing.Point(161, 95);
+            this.numericUpDown1.Location = new System.Drawing.Point(157, 68);
+            this.numericUpDown1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.numericUpDown1.Maximum = new decimal(new int[] {
             100000,
             0,
             0,
             0});
             this.numericUpDown1.Name = "numericUpDown1";
-            this.numericUpDown1.Size = new System.Drawing.Size(150, 27);
+            this.numericUpDown1.Size = new System.Drawing.Size(131, 23);
             this.numericUpDown1.TabIndex = 2;
             this.numericUpDown1.Value = new decimal(new int[] {
             100,
@@ -79,9 +82,10 @@
             // 
             // numericUpDown2
             // 
-            this.numericUpDown2.Location = new System.Drawing.Point(486, 95);
+            this.numericUpDown2.Location = new System.Drawing.Point(441, 68);
+            this.numericUpDown2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.numericUpDown2.Name = "numericUpDown2";
-            this.numericUpDown2.Size = new System.Drawing.Size(150, 27);
+            this.numericUpDown2.Size = new System.Drawing.Size(131, 23);
             this.numericUpDown2.TabIndex = 3;
             this.numericUpDown2.Value = new decimal(new int[] {
             5,
@@ -92,35 +96,35 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(45, 97);
+            this.label1.Location = new System.Drawing.Point(55, 70);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(110, 20);
+            this.label1.Size = new System.Drawing.Size(89, 15);
             this.label1.TabIndex = 4;
             this.label1.Text = "Ancho de Casa:";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(337, 97);
+            this.label2.Location = new System.Drawing.Point(311, 70);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(152, 20);
+            this.label2.Size = new System.Drawing.Size(121, 15);
             this.label2.TabIndex = 5;
             this.label2.Text = "Distancia entre Casas:";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(47, 586);
+            this.label3.Location = new System.Drawing.Point(27, 520);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(420, 20);
+            this.label3.Size = new System.Drawing.Size(334, 15);
             this.label3.TabIndex = 6;
             this.label3.Text = "Hecho por: Kristan Ruíz Limón y Diana Yulissa Sesma Santiago";
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(698, 629);
+            this.ClientSize = new System.Drawing.Size(658, 554);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
@@ -128,6 +132,7 @@
             this.Controls.Add(this.numericUpDown1);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "Form1";
             this.Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
